1299330ea08f2c4bb63a7042613b94abb443ecfc
Дмитрий Свиридов <dmitriisviridov675@yandex.ru>
2023-12-19 21:00:45 +0300

    fixed the error

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from random import randint
 
 
+from graphic_arts.start_game_banner import run_screensaver
+
+
 def attack(char_name: str, char_class: str) -> str:
     """Эта функция подсчитывает урон за определенный класс."""
     if char_class == 'warrior':
